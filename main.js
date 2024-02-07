03a3c401f6c0adf0f8a6d4d7ba29abe0c8a3988d
Miguel358976 <141787818+Miguel358976@users.noreply.github.com>
2024-02-07 20:02:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function events(){
+
+    window.location = "events.html";
+}
+function credits(){
+
+    window.location = "credits.html";
+}
+function back() {
+    window.location = "index.html";
+}
